26d2302ff4b181b23205e8db94aa0009822ef90a
Michael Kuzmyn <michael.kuzmyn@gmail.com>
2025-02-05 20:31:39 -0600

    app-list

--- src/types/app.ts
+++ src/types/app.ts
@@ -1,5 +1,6 @@
-export interface App {
-  AppName: string
-  Catalogs: string[]
-  Events: string[]
-}
+export type App = {
+  name: string;
+  description: string | null;
+  url: string | null;
+  icon: string | null;
+}; 
