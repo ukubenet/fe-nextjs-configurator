95d5cbf508415e508dc193581b564645531d85c1
Michael Kuzmyn <michael.kuzmyn@gmail.com>
2025-02-05 22:09:44 -0600

    addressing comments

--- src/types/app.ts
+++ src/types/app.ts
@@ -1,6 +1,5 @@
-export type App = {
-  name: string;
-  description: string | null;
-  url: string | null;
-  icon: string | null;
-}; 
+export interface App {
+  AppName: string
+  Catalogs: string[]
+  Events: string[]
+}
